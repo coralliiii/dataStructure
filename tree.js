1fd32c721a8c8bc7d91f5c803991842d38a6e77c
Coral <944284279@qq.com>
2023-02-01 09:22:19 +0800

    二叉树

--- tree.js
+++ tree.js
@@ -107,13 +107,66 @@ var postorderTraversal = function (root) {
     return result
 }
 
+function reConstructBinaryTree(pre,vin){
+    if(pre.length == 0){
+        return null
+    }
+    if(pre.length == 1){
+        return new Node(pre[0])
+    }
+    const value = pre[0]
+    console.log(value)
+    const index = vin.indexOf(value)
+    console.log('index',index)
+    const preLeft = pre.slice(1,index+1)
+    console.log('preLeft',preLeft)
+    const preRight = pre.slice(index+1)
+    const vinLeft = vin.slice(0,index)
+    const vinRight = vin.slice(index+1)
+    const node = new Node(value)
+    console.log('node',node)
+    node.left = reConstructBinaryTree(preLeft,vinLeft)
+    node.right = reConstructBinaryTree(preRight,vinRight)
+    return node
+}
+
+// 给定一棵二叉树的前序遍历和中序遍历，求其后序遍历
+function figurePost(...readline){
+    let pre
+    let vin 
+    let i = 0
+    while((pre = readline[i])!= null){
+        vin = readline[++i]
+        console.log('pre,vin:',pre,vin)
+        console.log(getPostOrder(pre,vin))
+        i++;
+    }
+    function getPostOrder(pre,vin){
+        if(pre.length == 0){
+            return ''
+        }
+        if(pre.length == 1){
+            return pre
+        }
+        let value = pre[0]
+        let index = vin.indexOf(value)
+        let preLeft = pre.slice(1,index+1)
+        let preRight = pre.slice(index+1)
+        let vinLeft = vin.slice(0,index)
+        let vinRight = vin.slice(index+1)
+        return getPostOrder(preLeft,vinLeft) + getPostOrder(preRight,vinRight) + value
+    }
+}
+
 var t = new Tree()
-t.insert(2)
-t.insert(4)
-t.insert(3)
-t.insert(8)
-t.insert(0)
+// t.insert(2)
+// t.insert(4)
+// t.insert(3)
+// t.insert(8)
+// t.insert(0)
 // console.log(JSON.stringify(t,null,2))
 // console.log(inorderTraversal(t.root))
 // console.log(preorderTraversal(t.root))
-console.log(postorderTraversal(t.root))
+// console.log(postorderTraversal(t.root))
+// console.log(JSON.stringify(reConstructBinaryTree([1,2,4,7,3,5,6,8],[4,7,2,1,5,3,8,6]),null,2))
+figurePost('ABC','BAC','FDXEAG','XDEFAG')
